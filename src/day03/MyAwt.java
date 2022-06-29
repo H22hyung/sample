6458d3d87674bff1c7dad1ad8075c164e5399e34
DESKTOP-7P4K98P\PC-21 <gmlwls0616@gmail.com>
2022-06-29 20:18:06 +0900

    첫번째 테스트입니다.

---
+++ src/day03/MyAwt.java
@@ -0,0 +1,13 @@
+package day03;
+
+import java.awt.Frame;
+
+public class MyAwt {
+	public static void main(String[] args) {
+		Frame f = new Frame();
+		f.setSize(400, 400); 
+//		f.setVisible(true); // 대기 타게...
+		f.setVisible(false); // 대기 타게...
+		
+	}
+}
